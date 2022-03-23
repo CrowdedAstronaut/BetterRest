c4e7948ab557bb8d53e07b4e75e84c7682b66c0b
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-23 10:36:33 -0500

    Challenge 1: Goodbye, VStack

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -20,23 +20,19 @@ struct ContentView: View {
 	
     var body: some View {
 		NavigationView {
-			VStack {
-				Text("When do you want to wake up?")
-					.font(.headline)
-
+			Section("When do you want to wake up?") {
 				DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
 					.labelsHidden()
+			}
 
-				Text("Desired amount of sleep")
-					.font(.headline)
-
+			Section("Desired amount of sleep") {
 				Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-				
-				Text("Daily coffee intake")
-					.font(.headline)
+			}
 
+			Section("Daily coffee intake") {
 				Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
 			}
+
 			.navigationTitle("BetterRest")
 			.toolbar {
 				Button("Calculate", action: calculateBedtime)
